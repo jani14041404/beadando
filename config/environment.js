101ac4904b2d1bb2d5798340a6a7400d090fd145
jani14041404 <janoseltealkfejl@gmail.com>
2016-01-18 14:50:41 +0100

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -24,7 +24,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self'",
       'font-src': "'self' 'unsafe-eval' https://fonts.gstatic.com",
-      'connect-src': "'self' 'unsafe-eval' http://alkfejl-rest-itamas.c9users.io",
+      'connect-src': "'self' 'unsafe-eval' 'https://restapi-jani1404.c9users.io'",
       'img-src': "'self'",
       'style-src': "'self' 'unsafe-eval' https://fonts.googleapis.com",
       'media-src': "'self'"
