7308289c029c769fea493844c4a8da29845603c4
jani14041404 <janoseltealkfejl@gmail.com>
2016-01-18 14:58:23 +0100

    First commit

--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,7 @@ module.exports = function(environment) {
       // when it is created
     }
   };
-  
+   
   ENV.contentSecurityPolicy = {
       'default-src': "'none'",
       'script-src': "'self'",
